f5386e22770902756031714afe162e4b5e491bb1
Ujvari Andras <ujvariandris@gmail.com>
2022-10-02 12:36:01 +0200

    Compatibility for django 4+

--- backend/CodeToGiveAPI/urls.py
+++ backend/CodeToGiveAPI/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf.urls import url, include
+from django.urls import re_path as url
+from django.conf.urls import include
 
 
 urlpatterns = [
