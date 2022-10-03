877d277de6683cd1c3b36dc4fc102bdde5f8ffd5
Schneider Marcell <78796219+Marci0707@users.noreply.github.com>
2022-10-03 14:29:08 +0200

    Burdon test

--- backend/CodeToGiveApp/models.py
+++ backend/CodeToGiveApp/models.py
@@ -18,3 +18,9 @@ class ToulousePieron(models.Model):
     user_hash = models.CharField(primary_key=True, max_length=32)
     correct_indices = models.CharField(max_length=1000, null=True, blank=True, default=None)
     results = models.CharField(max_length=750, null=True, blank=True, default=None)
+
+
+class Bourdon(models.Model):
+    user_hash = models.CharField(primary_key=True, max_length=32)
+    correct_indices = models.CharField(max_length=1000, null=True, blank=True, default=None)
+    results = models.CharField(max_length=750, null=True, blank=True, default=None)
--- backend/CodeToGiveApp/views.py
+++ backend/CodeToGiveApp/views.py
@@ -14,11 +14,11 @@ from rest_framework.views import APIView
 import numpy as np
 
 from . import models
-from .models import ChairLamp, User, ToulousePieron
+from .models import ChairLamp, User, ToulousePieron, Bourdon
 
 
 @dataclass
-class ChairLampResult:
+class IncrementalMetrics:
     quality_of_attention_total: float
     quality_of_attention_minutes: List[float]
     extent_of_attention: float
@@ -49,7 +49,7 @@ class ChairLampResult:
         return 100 - self.quality_of_attention_minutes[minute]
 
     def _get_category(self, performance: float, performance_map) -> str:
-        assert performance >= 0
+
         for minimum_performance in list(performance_map.keys()):
             if performance >= minimum_performance:
                 return performance_map[minimum_performance]
@@ -67,7 +67,7 @@ def check_can_start(test_model, *args, **kwargs):
                 if test_record.correct_indices is not None:
                     return Response('User Already Started The Test', status=400)
             except test_model.DoesNotExist as ex:
-                pass #If the user is not in the test's table, then they can start the test
+                pass  # If the user is not in the test's table, then they can start the test
             return http_method(cls, request, *args, **kwargs)
 
         return wrapper
@@ -93,7 +93,7 @@ def check_test_started(test_model, *args, **kwargs):
     return http_request_wrapper
 
 
-class MatrixModel:
+class IncrementalMatrixTest:
 
     def __init__(self, width: int, height: int, n_classes: int):
         self.width = width
@@ -103,20 +103,64 @@ class MatrixModel:
     def create_random_matrix(self):
         return np.random.randint(0, self.n_classes, (self.width, self.height), dtype=int)
 
+    def preprocess_and_validate(self, incrementally_marked_indices, correct_indices):
+
+        for minute_data in incrementally_marked_indices:
+            if not (np.diff(np.array(minute_data)) >= 0).all():
+                raise ValueError('Data not Incremental')
+        correct_indices = self.cap_correct_indices(incrementally_marked_indices, correct_indices)
+
+        return incrementally_marked_indices, correct_indices
+
+    def _get_incremental_metrics(self, errors: List[int], revised: List[int]) -> IncrementalMetrics:
+        quality_of_attention_total = round(sum(errors) / sum(revised) * 100, 2)
+
+        quality_of_attention_minutes = [round(errors[i] / revised[i] * 100, 2) for i in range(len(revised))]
+        extent_of_attention = max(revised) - min(revised)
+
+        result = IncrementalMetrics(quality_of_attention_total=quality_of_attention_total,
+                                    quality_of_attention_minutes=quality_of_attention_minutes
+                                    , extent_of_attention=extent_of_attention)
+
+        return result
+
     def get_1d_indices_of_classes(self, classes: Sequence[int], matrix: np.ndarray):
         return [idx for idx, value in enumerate((matrix.flatten())) if value in classes]
 
+    def cap_correct_indices(self, marked_indices, correct_indices):
+        last_marked_index = max(marked_indices[-1])
 
-class ChairLampEndpoint(APIView, MatrixModel):
+        capped = [idx for idx in correct_indices if idx <= last_marked_index]
+        return capped
+
+    def _evaluate_incremental_filling(self, incrementally_marked_indices: List[List[int]]
+                                      , correct_indices: List[int]
+                                      , ignored_indices: List[int] = ()):
+        revised = []
+        errors = []
+
+        for minute_idx, circled_indices in enumerate(incrementally_marked_indices):
+            prev_min = [] if minute_idx == 0 else incrementally_marked_indices[minute_idx - 1]
+            diff_from_previous_min = [idx for idx in circled_indices if idx not in prev_min]
+
+            error = [idx for idx in circled_indices if idx not in correct_indices + ignored_indices] + \
+                    [idx for idx in correct_indices if idx not in circled_indices + ignored_indices]
+
+            revised.append(max(diff_from_previous_min) - (max(prev_min) if prev_min != [] else 0))
+            errors.append(len(error))
+
+        return errors, revised
+
+
+class ChairLampEndpoint(APIView, IncrementalMatrixTest):
     correct_picture_indices = [0, 3]
 
     def __init__(self, **kwargs):
         APIView.__init__(self)
-        MatrixModel.__init__(self, width=3, height=4, n_classes=4)
+        IncrementalMatrixTest.__init__(self, width=3, height=4, n_classes=4)
 
     @check_can_start(test_model=ChairLamp)
     def get(self, request: Request, _format=None):
-
         user_id = request.path.split('/')[-1]
 
         random_matrix = self.create_random_matrix()
@@ -125,75 +169,45 @@ class ChairLampEndpoint(APIView, MatrixModel):
         ChairLamp(user_id, correct_indices).save()
         return Response(data={'matrix': random_matrix, "correct_indices": self.correct_picture_indices}, status=200)
 
-    def _get_metrics(self, errors: List[int], revised: List[int]) -> ChairLampResult:
-
-        quality_of_attention_total = round(sum(errors) / sum(revised) * 100, 2)
-
-        quality_of_attention_minutes = [round(errors[i] / revised[i] * 100, 2) for i in range(len(revised))]
-        extent_of_attention = max(revised) - min(revised)
-
-        result = ChairLampResult(quality_of_attention_total=quality_of_attention_total,
-                                 quality_of_attention_minutes=quality_of_attention_minutes
-                                 , extent_of_attention=extent_of_attention)
-
-        return result
-
-    def _evaluate_minutely_data(self, minutely_data: List[List[int]], correct_indices):
-
-        revised = []
-        errors = []
-
-        for minute_idx, circled_indices in enumerate(minutely_data):
-
-            if minute_idx < len(minutely_data) - 1:
-                n_revised_pictures = (min(minutely_data[minute_idx + 1]) + max(minutely_data[minute_idx])) // 2
-                revised.append(n_revised_pictures)
-            else:
-                revised.append(len(circled_indices))
-            errors.append(sum(1 for idx in circled_indices if idx not in correct_indices))
-
-        return errors, revised
-
     @check_test_started(test_model=ChairLamp)
     def post(self, request: Request, format=None):
-        """{"circled" : [[0,2], [0,2,4,9,10], [0,2,4,9,10,15]]} """
+        """{"circled" : [[0,2], [0,2,4,9,10], [0,2,4,9,10,15]],"finished_at":"date_string"}"""
 
         body = json.loads(request.body)
         user_id = request.path.split('/')[-1]
 
         chair_lamp_record = ChairLamp.objects.get(user_hash=user_id)
-
-
-        #take only revised indices into consideration
-        circled_indices_per_minute = body['circled']
+        marked_indices_per_minute = body['circled']
         correct_indices = json.loads(chair_lamp_record.correct_indices)
-        max_revised_index = np.max(circled_indices_per_minute)
-        correct_indices = [idx for idx in correct_indices if idx <= max_revised_index]
 
+        try:
+            marked_indices, correct_indices = self.preprocess_and_validate(marked_indices_per_minute, correct_indices)
+        except ValueError:
+            chair_lamp_record.correct_indices = None
+            chair_lamp_record.save()
+            return Response(data="Incorrect completion of test", status=400)
 
-        errors, revised = self._evaluate_minutely_data(circled_indices_per_minute, correct_indices)
+        errors, revised = self._evaluate_incremental_filling(marked_indices_per_minute, correct_indices)
+        metrics = self._get_incremental_metrics(errors, revised)
 
-        results = self._get_metrics(errors, revised)
-
-        chair_lamp_record.results = dataclasses.asdict(results)
+        metrics = dataclasses.asdict(metrics)
+        metrics['finished'] = body['finished']
+        chair_lamp_record.results = metrics
         chair_lamp_record.correct_indices = None
         chair_lamp_record.save()
         return Response(status=204)
 
 
-class ToulousePieronEndpoint(APIView, MatrixModel):
+class ToulousePieronEndpoint(APIView, IncrementalMatrixTest):
     correct_picture_indices = [1, 4, 5, 6]  # 0 = 0 degree rotation from 12 o'clock | 1 = 45degree | ..
 
     def __init__(self, **kwargs):
         APIView.__init__(self)
-        MatrixModel.__init__(self, width=20, height=20, n_classes=360 // 45)
+        IncrementalMatrixTest.__init__(self, width=20, height=20, n_classes=360 // 45)
 
     @check_can_start(test_model=ToulousePieron)
     def get(self, request: Request, _format=None):
         user_id = request.path.split('/')[-1]
-
-        if not User.objects.filter(user_hash=user_id).exists():
-            return Response('No such User', status=400)
 
         random_matrix = self.create_random_matrix()
         correct_indices = self.get_1d_indices_of_classes(self.correct_picture_indices, random_matrix)
@@ -203,28 +217,87 @@ class ToulousePieronEndpoint(APIView, MatrixModel):
 
     @check_test_started(test_model=ToulousePieron)
     def post(self, request: Request, format=None):
-        """{"circled" : [[0,2,4,9,10], [11,21,23], [40,43,51]]} """
-        user_id = request.path.split('/')[-1]
-        pieron_record = ToulousePieron.objects.get(user_hash=user_id)
-        final_circled_indices = json.loads(request.body)['circled'][-1]
+        """{"circled" : [[0,2], [0,2,4,9,10], [0,2,4,9,10,15]],"finished_at":"date_string"}"""
 
-        if not (np.diff(np.array(final_circled_indices)) >= 0).all():
+        user_id = request.path.split('/')[-1]
+        body = json.loads(request.body)
+        pieron_record = ToulousePieron.objects.get(user_hash=user_id)
+        marked_indices = body['circled'][-1]  # only check finally marked indices
+        correct_indices = json.loads(pieron_record.correct_indices)
+
+        try:
+            marked_indices, correct_indices = self.preprocess_and_validate([marked_indices], correct_indices)
+        except ValueError:
             pieron_record.correct_indices = None
             pieron_record.save()
             return Response(data="Incorrect completion of test", status=400)
 
-        correct_indices = json.loads(pieron_record.correct_indices)
+        errors, revised = self._evaluate_incremental_filling(marked_indices, correct_indices,
+                                                             ignored_indices=[
+                                                                 list(range(self.width))])  # ignore first row
 
-        # ignore first row and not revised
-        last_revised_index = max(final_circled_indices)
-        correct_indices = [idx for idx in correct_indices if idx <= last_revised_index]
-        not_circled = sum([1 for idx in correct_indices if idx not in final_circled_indices and idx >= self.width])
-        incorrectly_circled = sum([1 for idx in final_circled_indices if idx not in correct_indices and idx >= self.width])
+        errors = sum(errors)
+        revised = sum(revised)
 
-        errors = not_circled + incorrectly_circled
-        revised = (max(final_circled_indices) + 1) - self.width
-
-        pieron_record.results = {"accuracy": (revised - errors) / revised * 100}
+        pieron_record.results = {"accuracy": (revised - errors) / revised * 100, "finished" : body['finished']}
+        pieron_record.correct_indices = None
         pieron_record.save()
 
+        return Response(status=204)
+
+
+class BourdonEndpoint(APIView, IncrementalMatrixTest):
+
+    def __init__(self, **kwargs):
+        APIView.__init__(self)
+        IncrementalMatrixTest.__init__(self, width=40, height=40, n_classes=26)
+
+    @check_can_start(test_model=Bourdon)
+    def get(self, request: Request, _format=None):
+        user_id = request.path.split('/')[-1]
+
+        random_matrix = self.create_random_matrix()
+
+        flat_random_matrix = random_matrix.flatten()
+        correct_indices = []
+
+        for row_start_idx in range(0, self.width * self.height, self.width):
+            row = flat_random_matrix[row_start_idx:row_start_idx + self.width]
+            row_correct_indices = np.argwhere(row == flat_random_matrix[row_start_idx]).flatten() + row_start_idx
+            correct_indices += row_correct_indices.tolist()
+
+        Bourdon(user_id, correct_indices).save()
+        return Response(data={'matrix': random_matrix}, status=200)
+
+    @check_test_started(test_model=Bourdon)
+    def post(self, request: Request, format=None):
+        """{"circled" : [[0,2], [0,2,4,9,10], [0,2,4,9,10,15]],"finished_at":"date_string"}"""
+
+        user_id = request.path.split('/')[-1]
+        body = json.loads(request.body)
+        burdon_record = Bourdon.objects.get(user_hash=user_id)
+        marked_indices_per_minute = body['circled']
+        correct_indices = json.loads(burdon_record.correct_indices)
+
+        try:
+            marked_indices, correct_indices = self.preprocess_and_validate(marked_indices_per_minute, correct_indices)
+
+        except ValueError:
+            burdon_record.correct_indices = None
+            burdon_record.save()
+            return Response(data="Incorrect completion of test", status=400)
+
+        # ignore first column (those are the labels)
+        ignored_indices = list(range(0, self.width * self.height, self.width))
+        errors, revised = self._evaluate_incremental_filling(marked_indices_per_minute, correct_indices,
+                                                             ignored_indices)
+        print(correct_indices)
+        print(ignored_indices,errors,revised)
+
+        metrics = self._get_incremental_metrics(errors, revised)
+        metrics = dataclasses.asdict(metrics)
+        metrics['finished'] = body['finished']
+        burdon_record.results = metrics
+        burdon_record.correct_indices = None
+        burdon_record.save()
         return Response(status=204)
