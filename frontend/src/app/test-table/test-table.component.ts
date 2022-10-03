dfbb410e170512e4e93bcac28fcdbf23357a141f
Ujvari Andras <ujvariandris@gmail.com>
2022-10-03 08:40:37 +0200

    Exception handling, improved improved error handling

--- frontend/src/app/test-table/test-table.component.ts
+++ frontend/src/app/test-table/test-table.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpResponse} from "@angular/common/http";
 import {Config, ConfigService} from "../config/config.service";
 import {Observable, range, Subscription} from "rxjs";
 import { timer } from "rxjs";
@@ -10,7 +10,7 @@ import { timer } from "rxjs";
   styleUrls: ['./test-table.component.css']
 })
 
-export class TestTableComponent implements OnInit {
+export class TestTableComponent implements OnDestroy {
   matrix:Array<Array<Number>> =[]
   results:Array<Array<Number>> =[]
   results_row:Array<Number>=[-1]
@@ -18,41 +18,60 @@ export class TestTableComponent implements OnInit {
   test_finished=false
   timerDisplay=""
   time=0
+  descriprion="IDE JÖN A LEÍRÁS"
   offlineUrl = 'assets/matrix.json';
   timer: Subscription | undefined
+  http_sub: Array<Subscription>=[]
+  res_post: Subscription | undefined
+
   errorText: string | null | undefined
   onLoadClick(){
-
-      let res=this.http.get<Config>('http://127.0.0.1:8000/chairlamp/bfe07c76562eb1fccb970ed3b30800d5',
-      { responseType: 'json', observe: "response",headers:{}}).subscribe( data => {
-        if((+data.status)==200){
-          this.matrix = data.body!.matrix
-
-          //console.log(this.matrix)
-          this.matrix_loaded = true
-          this.startTimer()
-        }else{
-          this.errorText=data.statusText
-
-        }
-
-      })
+    try {
+      this.http_sub?.push(this.http.get<Config>('http://127.0.0.1:8000/chairlamp/bfe07c76562eb1fccb970ed3b30800d5',
+        {responseType: 'json', observe: "response", headers: {}}).subscribe(data =>
+        this.processGetResponse(data)))
+    }
+    catch (e) {
+            this.errorText="HOGY KELL EXCEPTIONBOL STRINGET KISZEDNI????"
+    }
   }
+
+  private processGetResponse(data: HttpResponse<Config>) {
+    if ((+data.status) == 200) {
+      this.matrix = data.body!.matrix
+      this.matrix_loaded = true
+      this.startTimer()
+    } else {
+      this.errorText = data.statusText
+    }
+  }
+
   submit(){
-    this.matrix_loaded = false
     this.results.push(this.results_row)
     this.timer?.unsubscribe()
-    this.http.post<string>('http://127.0.0.1:8000/chairlamp/bfe07c76562eb1fccb970ed3b30800d5',
-      {circled: this.results, finished_at:this.time}, {observe:"response"}).subscribe((data)=>{
-      console.log(data.status)
-      if(data.status==200) {
-        this.test_finished = true
-        console.log(this.results)
-      }else{
-        this.errorText=data.statusText
-      }
-    })
+    try {
+      this.http_sub?.push(this.http.post<string>('http://127.0.0.1:8000/chairlamp/bfe07c76562eb1fccb970ed3b30800d5',
+        {circled: this.results, finished_at: this.time}, {observe: "response"}).subscribe(
+        (data) => this.processPostResponse(data)))
+    }catch (e){
+      this.matrix_loaded=false
+      this.errorText="HOGY KELL EXCEPTIONBOL STRINGET KISZEDNI????"
+    }
   }
+
+  private processPostResponse(data: HttpResponse<string>) {
+    console.log(data.status)
+    this.matrix_loaded = false
+    if (data.status == 200) {
+      this.test_finished = true
+      console.log(this.results)
+    } else if (data.status == 400) {
+      this.errorText = "User not in db (will be sent from server later)"
+    } else {
+      this.errorText = data.statusText
+    }
+  }
+
   onImageClick(col:number, row:number){
     //elmenti, hogy be van karikázva:
     let row_length=this.matrix[0].length
@@ -66,10 +85,17 @@ export class TestTableComponent implements OnInit {
   constructor(private http : HttpClient) {
   }
 
+  ngOnDestroy(): void {
+    console.log("OnDestroy")
+    for (let httpSubElement of this.http_sub) {
+      httpSubElement.unsubscribe()
+    }
+    this.timer?.unsubscribe()
+  }
+
   getDisplayTimer(time: number) {
     const minutes = ''+Math.floor(time % 3600 / 60);
     const seconds = '0' + Math.floor(time % 3600 % 60);
-
     return minutes.slice(-2, -1 ) + minutes.slice(-1) +":"+
       seconds.slice(-2, -1 ) + seconds.slice(-1)
 
